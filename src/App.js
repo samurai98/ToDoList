48ac13eed34e24f7b66df433e581327b58710238
samurai98 <kirito1498@gmail.com>
2020-04-12 00:36:39 +0300

    feat: add props lesson

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './App.css';
 import ToDoListHeader from "./ToDoListHeader";
-import ToDoListTask from "./ToDoListTask";
+import ToDoListTasks from "./ToDoListTasks";
 import ToDoListFooter from "./ToDoListFooter";
 
 
 class App extends React.Component {
+    tasks = [
+        {title: "CSS", isDone: true, priority: "low"},
+        {title: "JS", isDone: false, priority: "low"},
+        {title: "ReactJS", isDone: true, priority: "low"},
+        {title: "jQuery", isDone: false, priority: "low"}
+    ];
+
+    filterValue = "Completed";
+
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
-                    <ToDoListHeader />
-                    <ToDoListTask />
-                    <ToDoListFooter />
+                    <ToDoListHeader/>
+                    <ToDoListTasks tasks={this.tasks}/>
+                    <ToDoListFooter filterValue={this.filterValue}/>
                 </div>
             </div>
         );
