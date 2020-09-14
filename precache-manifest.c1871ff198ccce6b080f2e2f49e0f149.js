c8cb11c72045f93650efabbd639f5fddb44043e6
samurai98 <kirito1498@gmail.com>
2020-09-14 18:59:59 +0300

    Updates

---
+++ precache-manifest.c1871ff198ccce6b080f2e2f49e0f149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb20726c2b4048ee9f8181380ae2ff3",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "2dfa45c96dbe3a16e742",
+    "url": "/ToDoList/static/css/main.cb49f3c8.chunk.css"
+  },
+  {
+    "revision": "b6e80b4a04a095eb71a0",
+    "url": "/ToDoList/static/js/2.d3a45c49.chunk.js"
+  },
+  {
+    "revision": "2dfa45c96dbe3a16e742",
+    "url": "/ToDoList/static/js/main.0553b742.chunk.js"
+  },
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  },
+  {
+    "revision": "19208ba09acf5eea0aa4f17cbbfccdee",
+    "url": "/ToDoList/static/media/background.19208ba0.jpg"
+  }
+]);
